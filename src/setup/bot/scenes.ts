30c835a26dd893720c8e87800c7fee3e3e77a30a
Roman Davydenko <deadshot323rd@yandex.ru>
2024-03-07 11:45:10 +0800

    feat: add generate images

--- src/setup/bot/scenes.ts
+++ src/setup/bot/scenes.ts
@@ -1,7 +1,8 @@
 import { Scenes } from 'telegraf';
-import { stepStart, generateByTextStep } from '../../views';
+import { stepStart, generateByTextStep1, generateByTextStep2, generateByTextStep3 } from '../../views';
 
 const startScene = new Scenes.WizardScene<Scenes.WizardContext>('startScene', stepStart);
-const generateByTextScene = new Scenes.WizardScene<Scenes.WizardContext>('generateByTextScene', generateByTextStep);
+const generateByTextScene = new Scenes.WizardScene<Scenes.WizardContext>('generateByTextScene', generateByTextStep1, generateByTextStep2, generateByTextStep3);
+
 
 export const stage = new Scenes.Stage<Scenes.WizardContext>([startScene, generateByTextScene]);
