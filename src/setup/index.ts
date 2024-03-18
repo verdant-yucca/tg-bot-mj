28d39716202842ed730dc1606967b946b1726534
Roman Davydenko <Deadshot323rd@yandex.ru>
2024-03-18 22:06:22 +0800

    feat: start server with  another solution

--- src/setup/index.ts
+++ src/setup/index.ts
@@ -3,9 +3,15 @@ import cors from 'cors';
 import { json, urlencoded } from 'body-parser';
 import { join } from 'path';
 import { reloadJson } from '../constants/messages';
+import app from '../index';
+import { IConfigService } from '../config/config.interface';
+import { ConfigService } from '../config/config.service';
+import { prettyLog } from '../utils';
 
 export const setupApp = () => {
     const app: Application = express();
+    const config: IConfigService = new ConfigService();
+
     app.use(cors());
     app.use(json());
     app.use(urlencoded({ extended: true }));
@@ -17,5 +23,14 @@ export const setupApp = () => {
         res.send({ result: true })
     });
     app.get('/', (_, res) => res.sendFile(join(__dirname + '/static/index.html')));
+    const port = config.get('PORT');
+    console.log('port', port);
+
+    app.listen(port, () => console.log(`App listening on port ${port}`))
+
+    prettyLog({
+        message: `Server running at http://localhost::${port}`,
+        options: { padding: 1, borderColor: 'green' }
+    });
     return app;
 };
