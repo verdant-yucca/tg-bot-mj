62a4778ca72f3adea148397db1bc1048aa943bbf
Roman Davydenko <Deadshot323rd@yandex.ru>
2024-03-18 21:56:05 +0800

    feat: start server with  another solution

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,22 @@ import { IConfigService } from './config/config.interface';
 import { ConfigService } from './config/config.service';
 import { setupApp } from './setup';
 import { setupBot } from './setup/bot';
+import { prettyLog } from './utils';
 
 dotenv.config();
 const app = setupApp();
 const config: IConfigService = new ConfigService();
 
 const token = config.get('BOT_TOKEN');
+const port = config.get('PORT');
+
 setupBot(token);
+
+app.listen(port, () => console.log(`App listening on port ${port}`))
+
+prettyLog({
+    message: `Server running at http://localhost::${port}`,
+    options: { padding: 1, borderColor: 'green' }
+});
 
 export default app;
