ecc22b491a1c1b8643ce3fd28df1b2cc12bc05a5
Roman Davydenko <Deadshot323rd@yandex.ru>
2024-03-18 20:49:08 +0800

    feat: start server with  another solution

--- index.ts
+++ index.ts
@@ -1,5 +1,3 @@
-import http from 'http';
-
 import { IConfigService } from './src/config/config.interface';
 import { ConfigService } from './src/config/config.service';
 import app from './src';
@@ -8,8 +6,7 @@ import { prettyLog } from './src/utils/prettyLog.util';
 const config: IConfigService = new ConfigService();
 const port = config.get('PORT');
 
-http.createServer({}, app).listen(port);
-
+app.listen(port, () => console.log(`App listening on port ${port}`))
 prettyLog({
   message: `Server running at http://localhost::${port}`,
   options: { padding: 1, borderColor: 'green' }
