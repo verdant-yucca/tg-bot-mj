a3a8bb6dba186c21955682511930cc0f15e72ae6
Roman Davydenko <Deadshot323rd@yandex.ru>
2024-04-05 16:07:10 +0800

    feat: added queues

--- src/utils/formatingPrompt.ts
+++ src/utils/formatingPrompt.ts
@@ -1,7 +1,1 @@
-export const formatingPrompt = (prompt: string) => {
-    let updatedPrompt = prompt;
-    updatedPrompt = updatedPrompt.toLowerCase();
-    updatedPrompt = updatedPrompt.replace('style', '');
-    updatedPrompt = updatedPrompt.replace('--', '');
-    return updatedPrompt;
-};
+export const formatingPrompt = (prompt: string) => prompt.toLowerCase().replace('style', '').replace('-', '');
