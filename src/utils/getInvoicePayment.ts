b5217414e4a9e57a55e2e0278f93dd4a43303ab8
Roman Davydenko <deadshot323rd@yandex.ru>
2024-04-23 10:32:22 +0800

    feat: small fix

--- src/utils/getInvoicePayment.ts
+++ src/utils/getInvoicePayment.ts
@@ -2,15 +2,14 @@ import { getPackages } from './db/packages';
 
 export const getInvoicePayments = async (id: number): Promise<any[]> => {
     const { packages } = await getPackages();
-    const offer = 1;
     return packages.map(({ count, name, price, title, description, photoUrl, photoWidth, photoHeight }) => ({
         chat_id: id, // Уникальный идентификатор целевого чата или имя пользователя целевого канала
-        provider_token: process.env.PROVIDER_TOKEN, // токен выданный через бот @SberbankPaymentBot
+        provider_token: process.env.PROVIDER_TOKEN, // токен выданный через бот юкассы
         start_parameter: 'get_access', //Уникальный параметр глубинных ссылок. Если оставить поле пустым, переадресованные копии отправленного сообщения будут иметь кнопку «Оплатить», позволяющую нескольким пользователям производить оплату непосредственно из пересылаемого сообщения, используя один и тот же счет. Если не пусто, перенаправленные копии отправленного сообщения будут иметь кнопку URL с глубокой ссылкой на бота (вместо кнопки оплаты) со значением, используемым в качестве начального параметра.
         title: title, // Название продукта, 1-32 символа
         description: description, // Описание продукта, 1-255 знаков
         currency: 'RUB', // Трехбуквенный код валюты ISO 4217
-        prices: [{ label: name, amount: price * 100 * (offer || 1) }], // Разбивка цен, сериализованный список компонентов в формате JSON 100 копеек * 100 = 100 рублей
+        prices: [{ label: name, amount: price * 100 }], // Разбивка цен, сериализованный список компонентов в формате JSON 100 копеек * 100 = 100 рублей
         photo_url: photoUrl, // URL фотографии товара для счета-фактуры. Это может быть фотография товара или рекламное изображение услуги. Людям больше нравится, когда они видят, за что платят.
         photo_width: photoWidth, // Ширина фото
         photo_height: photoHeight, // Длина фото
@@ -22,10 +21,10 @@ export const getInvoicePayments = async (id: number): Promise<any[]> => {
                     description: `Покупка ${title} в телеграм боте`,
                     quantity: '1.00',
                     measure: 'piece',
-                    payment_subject: 'service', // 'commodity' || 'service' = Товар || Услуга
+                    payment_subject: 'service', // Услуга
                     payment_mode: 'full_payment',
                     amount: {
-                        value: `${(price * (offer || 1))}.00`,
+                        value: `${price}.00`,
                         currency: 'RUB'
                     },
                     vat_code: 1
