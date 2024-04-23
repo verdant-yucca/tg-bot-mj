2416c3e29670149f7751d0151c03d28d7d410505
Roman Davydenko <Deadshot323rd@yandex.ru>
2024-04-23 20:28:41 +0800

    feat: add few console log

--- src/utils/compressImage.ts
+++ src/utils/compressImage.ts
@@ -5,16 +5,14 @@ export const compressImage = async (imageUrl: string): Promise<Buffer | 1> => {
     try {
         // Качество сжатия (от 0 до 100, где 100 - наилучшее качество)
         const quality = 80;
+        console.log('start compress', imageUrl);
 
         // Загрузка изображения по URL
         const response = await axios.get(imageUrl, { responseType: 'arraybuffer' });
 
         // Сжатие изображения с потерей качества
-        return await sharp(response.data)
-            .jpeg({ quality: quality })
-            .toBuffer();
+        return await sharp(response.data).jpeg({ quality: quality }).toBuffer();
     } catch {
         return 1;
     }
-
 };
